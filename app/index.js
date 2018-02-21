16e3d7d651fb5d6e2d8f48388a7be637189d7e8a
kerry.wang <wrasdf@gmail.com>
2018-02-21 22:54:09 +1100

    WIP 1

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(`Hello World! -- ${process.env.NODE || 'undefind'}`)
 })
 
 app.get('/health', function (req, res) {
@@ -12,7 +12,8 @@ app.get('/health', function (req, res) {
 app.get('/metadata', function (req, res) {
   res.send(JSON.stringify({
     "version": "v0.0.1",
-    "author": "kerry"
+    "author": "kerry",
+    "node": `${process.env.NODE || "undefind"}`
   }))
 })
 
